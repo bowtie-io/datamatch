3de0ea38e3e37b2d3235a0de69047236ce8f1fc4
James Kassemi <jkassemi@gmail.com>
2015-04-30 13:42:42 -0600

    Route configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  resources :matches
+  # Completed matches for the current user
+  get '/matches'            => 'matches#index'
+
+  # Completed matches for the current user that the user has not been notified of
+  get '/matches/unnotified' => 'matches#unnotified'
+
+  # Potential matches to display to the current user for selection
+  get '/matches/potential'  => 'matches#potential'
+
+  # Create a new match for the current user
+  post '/matches'           => 'matches#create'
+
+  # Handle user profile data updates and any other BowTie web hooks
   post '/webhooks/bowtie' => 'bowtie_webhooks#create'
 end
